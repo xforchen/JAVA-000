6ff4926a59fb6089ef06fc82972a5fe6bf209b64
xforchen <xforchen@163.com>
2020-10-28 23:53:38 +0800

    week02

--- Week_02/HttpClientDemo.java
+++ Week_02/HttpClientDemo.java
@@ -1,4 +1,13 @@
-package com.xforchen.demo.httpclient;
+package com.example.demo.httpclient;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 
 /**
  * @author xforchen
